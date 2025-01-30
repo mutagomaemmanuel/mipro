6829870d8d3e4816e79381d7c37a789d88772727
mutagomaemmanuel <mutagomaemmanuel654@gmail.com>
2025-01-30 13:31:20 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  
+runApp(MaterialApp(
+  home:Center(
+    child:Text('Mutagoma emmanuel',style: TextStyle(color: const Color.fromARGB(255, 179, 186, 192)),),
+  )
+  
+));
+}
